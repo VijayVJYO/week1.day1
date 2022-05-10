4f96d060dbc8958a9baafb37258d0240426e8c76
VijayVJYO <105290970+VijayVJYO@users.noreply.github.com>
2022-05-10 18:13:55 +0530

    Add files via upload

---
+++ Twowheeler.java
@@ -0,0 +1,21 @@
+package selenium;
+
+public class Twowheeler{
+
+	      int noOfWheels=2;
+	      short noOfMirrors=2;
+	      long chasissNumber=742724547724l;
+	      boolean isPunchured=false;
+	      String bikeName="ktm";
+	      double runningKM=2.43546;
+	      
+		public static void main(String[] args) {
+		      Twowheeler obj=new Twowheeler();
+	        System.out.println(obj.noOfWheels);
+	        System.out.println(obj.noOfMirrors);
+	        System.out.println(obj.isPunchured);
+	        System.out.println(obj.bikeName);
+	        System.out.println(obj.runningKM);
+		}
+
+	}
